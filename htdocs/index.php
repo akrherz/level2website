3d8229b0014a6d1a8d94dac72cb0576f9f5f5d2e
akrherz <akrherz@iastate.edu>
2016-08-31 14:19:34 -0500

    add robots and more homepage content

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,23 @@
 </head>
 <body>
 
-<a href="/level2/raw">Archive Level II Files</a>
+<p><a href="/level2/raw/">Archive Level II Files</a>
+
+<p><pre>
+http://mesonet-nexrad.agron.iastate.edu/level2/raw/
+</pre>
+
+<p>Contacts:
+<br />daryl herzmann:
+<br />Twitter: <a href="https://twitter.com/akrherz">@akrherz</a>
+<br />Email: akrherz@iastate.edu
+<br />Phone: 515-294-5978
+</p>
+
+<p>Usage Restrictions:
+<br />There are none.  Please don't sue us and also try to have your 
+applications use the dir.list files with partial HTTP requests for
+efficient polling.</p>
 
 </body>
 </html>
