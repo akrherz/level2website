29b3111c28ec5677654d17d9ce0ed4cd0ed2ecc4
akrherz <akrherz@iastate.edu>
2019-04-02 22:53:04 -0500

    add fetch of KULM RADAR

---
+++ scripts/kulm_fetch.py
@@ -0,0 +1,33 @@
+"""Download KULM data."""
+import json
+import os
+import subprocess
+
+import requests
+BASE = "http://wxdata.geos.ulm.edu/radar/KULM/"
+LOCAL = "/mnt/level2/raw/KULM/"
+
+
+def main():
+    """Go Main Go."""
+    secrets = json.load(open('kulm_secrets.json'))
+    auth = (secrets['username'], secrets['password'])
+    req = requests.get(BASE + "dir.list", auth=auth, timeout=10)
+    for line in req.text.split("\n"):
+        tokens = line.split()
+        if len(tokens) != 2 or not tokens[1].startswith("KULM"):
+            continue
+        fn = tokens[1].strip()
+        if os.path.isfile(LOCAL + fn):
+            continue
+        req2 = requests.get(BASE + fn, auth=auth, timeout=20)
+        fp = open(LOCAL + fn, 'wb')
+        fp.write(req2.content)
+        fp.close()
+
+    os.chdir(LOCAL)
+    subprocess.call("/local/ldm/pyWWA/util/gr.csh KULM", shell=True)
+
+
+if __name__ == '__main__':
+    main()
