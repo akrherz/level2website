4109c8ca1ac53487c1265abffeb87643ba469c03
akrherz <akrherz@iastate.edu>
2019-04-04 20:31:38 -0500

    insert KULM into LDM

--- scripts/kulm_fetch.py
+++ scripts/kulm_fetch.py
@@ -13,6 +13,7 @@ def main():
     secrets = json.load(open('kulm_secrets.json'))
     auth = (secrets['username'], secrets['password'])
     req = requests.get(BASE + "dir.list", auth=auth, timeout=10)
+    files = []
     for line in req.text.split("\n"):
         tokens = line.split()
         if len(tokens) != 2 or not tokens[1].startswith("KULM"):
@@ -21,12 +22,16 @@ def main():
         if os.path.isfile(LOCAL + fn):
             continue
         req2 = requests.get(BASE + fn, auth=auth, timeout=20)
+        files.append(fn)
         fp = open(LOCAL + fn, 'wb')
         fp.write(req2.content)
         fp.close()
 
     os.chdir(LOCAL)
     subprocess.call("/local/ldm/pyWWA/util/gr.csh KULM", shell=True)
+    for fn in files:
+        subprocess.call(
+            "/home/ldm/bin/pqinsert -i -f NEXRAD2 %s" % (fn, ), shell=True)
 
 
 if __name__ == '__main__':
