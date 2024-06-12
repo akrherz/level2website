0d9f3ede3e833bd0ce14e5b03e4ba01be22edc4d
akrherz <akrherz@iastate.edu>
2024-06-12 07:14:00 -0500

    sync upstream scripts

--- scripts/wilu_fetch.py
+++ scripts/wilu_fetch.py
@@ -7,33 +7,45 @@ import sys
 
 import requests
 
-BASE = "http://wiu.edu/users/severewe/radar/nexrad2/WILU/"
+BASE = "http://wiu.edu/SevereWeather/radar/nexrad2/WILU/"
 LOCAL = "/mnt/level2/raw/WILU/"
 
 
 def make_request(url):
     """Safer."""
     try:
-        return requests.get(url, timeout=20)
+        return requests.get(url, timeout=10)
     except socket.timeout:
         print(f"socket.timeout for {url}")
-    except Exception as exp:
-        print(f"exception {exp} for {url}")
+    except Exception:
+        try:
+            return requests.get(url, timeout=10)
+        except requests.exceptions.ReadTimeout:
+            return None
+        except Exception as exp:
+            print(f"exception {exp} for {url}")
     sys.exit()
 
 
 def main():
     """Go Main Go."""
     req = make_request(BASE + "dir.list")
+    if req is None:
+        return
     files = []
-    for line in req.text.split("\n"):
+    for lnum, line in enumerate(req.text.split("\n")[::-1]):
+        if len(files) > 1 or lnum > 20:
+            break
         tokens = line.split()
         if len(tokens) != 2 or not tokens[1].startswith("WILU"):
             continue
-        fn = tokens[1].strip()
+        fn2 = tokens[1].strip()
+        fn = tokens[1].strip()[:18]
         if os.path.isfile(LOCAL + fn):
             continue
-        req2 = make_request(BASE + fn)
+        req2 = make_request(BASE + fn2)
+        if req2 is None:
+            return
         files.append(fn)
         with open(LOCAL + fn, "wb") as fh:
             fh.write(req2.content)
